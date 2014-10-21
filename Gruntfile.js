383c252e42ef5213849ffb6369759ab9755a84fa
gpspake <gpspake@gmail.com>
2014-10-21 18:38:55 -0500

    Create uthsc.scss
    Move styles in to uthsc.scss and refactor
    Update grunt file to watch uthsc.scss
    Update template to point to uthsc.scss

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
           outputStyle: 'compressed'
         },
         files: {
-          'css/app.css': 'scss/app.scss'
+          'css/uthsc.css': 'scss/uthsc.scss'
         }        
       }
     },
