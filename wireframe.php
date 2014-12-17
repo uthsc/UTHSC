b1ff843feb22dfa89c07a772bd3be875aec8a01e
gpspake <gpspake@gmail.com>
2014-12-17 12:46:08 -0600

    pull in wireframe menu markup

--- wireframe.php
+++ wireframe.php
@@ -33,8 +33,21 @@
 
 <!-- Left Off Canvas Menu -->
 <nav class="offcanvas-nav menu push-menu-left">
+    <!--mobile Breadcrumbs-->
+    <a class="button uthsc-split-button-home" href="/"><i class="fa fa-home"></i></a>
+    <button data-dropdown="breadcrumbs-left" aria-controls="drop", aria-expanded="false" class="button dropdown uthsc-split-button">Breadcrumbs Back Home</button><br>
+    <ol aria-labelledby="breadcrumblabel" id="breadcrumbs-left" data-dropdown-content class="f-dropdown mega uthsc-split-button-breadcrumb-links" role="menu" aria-hidden="false" tabindex="-1">
+        <li><a href="/" title="Home"><i class="fa fa-level-up fa-2x fa-flip-horizontal" style="float: left;"></i> Back to the Homepage</a></li>
+        <li><a href="products.html" title="Products">College of Medicine</a></li>
+        <li><a href="products.html" title="Products">Office of Medical Education</a></li>
+        <li><a href="products.html" title="Products">Clerkships</a></li>
+        <li><a href="products.html" title="Products">Core Clerkship Directors</a></li>
+        <li><a href="" class="current">Chattanooga</a></li>
+    </ol>
+    <input type="text" placeholder="Search" style="margin:0;">
+
+    <button class="close-menu expand"><i class="fa fa-chevron-left"></i> Close Menu &emsp;</button>
     <ul>
-        <li><button class="close-menu">&larr; Close</button></li>
         <li><a href="#">Broccoli</a></li>
         <li><a href="#">Tomato</a></li>
         <li><a href="#">Cucumber</a></li>
@@ -43,18 +56,124 @@
         <li><a href="#">CucumberCucu mberCucumbe rCucumber CucumberCucumber</a></li>
         <li><a href="#">Kale</a></li>
         <li><a href="#">Celery</a></li>
-
     </ul>
+    <button class="close-menu expand"><i class="fa fa-chevron-left"></i> Close Menu &emsp;</button>
 </nav>
 
 <!-- Right Off Canvas Menu -->
 <nav class="offcanvas-nav menu push-menu-right">
-
-    <input type="text" placeholder="Search">
+    <!--mobile Breadcrumbs-->
+    <a class="button uthsc-split-button-home" href="/"><i class="fa fa-home"></i></a>
+    <button data-dropdown="breadcrumbs-right" aria-controls="drop", aria-expanded="false" class="button dropdown uthsc-split-button">Breadcrumbs Back Home</button><br>
+    <ol aria-labelledby="breadcrumblabel" id="breadcrumbs-right" data-dropdown-content class="f-dropdown mega uthsc-split-button-breadcrumb-links" role="menu" aria-hidden="false" tabindex="-1">
+        <li><a href="/" title="Home"><i class="fa fa-level-up fa-2x fa-flip-horizontal" style="float: left;"></i> Back to the Homepage</a></li>
+        <li><a href="products.html" title="Products">College of Medicine</a></li>
+        <li><a href="products.html" title="Products">Office of Medical Education</a></li>
+        <li><a href="products.html" title="Products">Clerkships</a></li>
+        <li><a href="products.html" title="Products">Core Clerkship Directors</a></li>
+        <li><a href="" class="current">Chattanooga</a></li>
+    </ol>
+    <input type="text" placeholder="Search" style="margin:0;">
+    <button class="close-menu expand">&emsp; Close Menu &emsp;<i class="fa fa-chevron-right"></i></button>
+    <!--Mission Links-->
+    <div class="mission-links">
+        <h2>Mission</h2>
+        <a class="button inverse" href="/education/"><i class="fa fa-graduation-cap"></i> Education</a>
+        <a class="button inverse" href="/research/"><i class="fa fa-flask"></i> Research</a>
+        <a class="button inverse" href="/clinicalcare/"><i class="fa fa-medkit"></i> Clinical Care</a>
+        <a class="button inverse" href="/publicservice/"><i class="fa fa-globe"></i> Public Service</a>
+    </div>
+    <!--Education
+        <a class="button uthsc-split-button-home inverse" href="/education/"><i class="fa fa-graduation-cap"></i></a>
+        <button data-dropdown="mission-education" aria-controls="drop", aria-expanded="false" class="button dropdown uthsc-split-button inverse">Education</button><br>
+            <ol aria-labelledby="breadcrumblabel" id="mission-education" data-dropdown-content class="f-dropdown mega uthsc-split-button-mission-links inverse" role="menu" aria-hidden="false" tabindex="-1">
+                <li><i class="fa fa-level-up fa-flip-horizontal"></i><a href="/education/" title="Home">Go to the Education Site</a></li>
+                <li><a href="/dentistry/">Dentistry</a></li>
+                <li><a href="/grad/">Graduate Health Sciences</a></li>
+                <li><a href="/health-professions/">Health Professions</a></li>
+                <li><a href="/Medicine/">Medicine</a></li>
+                <li><a href="/nursing/">Nursing</a></li>
+                <li><a href="/pharmacy/">Pharmacy</a></li>
+            </ol>
+    Education (end)-->
+    <!--Research
+        <a class="button uthsc-split-button-home inverse" href="/research/"><i class="fa fa-flask"></i></a>
+        <button data-dropdown="mission-research" aria-controls="drop", aria-expanded="false" class="button dropdown uthsc-split-button inverse">Research</button><br>
+            <ol aria-labelledby="breadcrumblabel" id="mission-research" data-dropdown-content class="f-dropdown mega uthsc-split-button-mission-links inverse" role="menu" aria-hidden="false" tabindex="-1">
+                <li><i class="fa fa-level-up fa-flip-horizontal"></i><a href="/research/" title="Home">Go to the Research Site</a></li>
+                    <li><a href="/research/vc_research.php">Vice Chancellor for Research</a></li>
+                    <li><a href="/research/about.php">About Research at UTHSC</a></li>
+                    <li><a href="/research/research_administration/">Office of Research Administration</a></li>
+                    <li><a href="/research/research_compliance/">Office of Research Compliance</a></li>
+                    <li><a href="/research/research_administration/research_contracts/">Research Contracts</a></li>
+                    <li><a href="/research/research_programs/">Research Centers and Programs</a></li>
+                    <li><a href="/research/research_resources/">Research Resources</a></li>
+                    <li><a href="/postdoc/">Postdoctoral Office</a></li>
+                    <li><a href="/research/student_research_programs/">Student Research Programs</a></li>
+                    <li><a href="/research/utrf.php">Technology Transfer</a></li>
+                    <li><a href="/research/MathWorks/">MathWorks Software</a></li>
+            </ol>
+    Research (end)-->
+    <!--Clinical Care
+        <a class="button uthsc-split-button-home inverse" href="/clinicalcare/"><i class="fa fa-medkit"></i></a>
+        <button data-dropdown="mission-clinicalcare" aria-controls="drop", aria-expanded="false" class="button dropdown uthsc-split-button inverse">Clinical Care</button><br>
+            <ol aria-labelledby="breadcrumblabel" id="mission-clinicalcare" data-dropdown-content class="f-dropdown mega uthsc-split-button-mission-links inverse" role="menu" aria-hidden="false" tabindex="-1">
+                <li><i class="fa fa-level-up fa-flip-horizontal"></i><a href="/clinicalcare/" title="Home">Go to the Clinical Care Site</a></li>
+                <li><a href="http://www.utmedicalgroup.com/">UT Medical Group Home</a></li>
+                <li><a href="http://www.utmedicalgroup.com/AboutUs/">About UT Medical Group</a></li>
+                <li><a href="http://www.utmedicalgroup.com/Doctors/search.php">Find a Doctor</a></li>
+                <li><a href="http://www.utmedicalgroup.com/PatientResources/index.php?pgID=15">Patient Resources</a></li>
+                <li><a href="http://www.utmedicalgroup.com/Departments/">Specialties Offered</a></li>
+                <li><a href="/dentistry/patients/spec_clinics.php">Emergencies and Specialty Clinics</a></li>
+                <li><a href="/dentistry/patients/">General Information</a></li>
+                <li><a href="/dentistry/omds/">Oral &amp; Maxillofacial Diagnostic Services</a></li>
+                <li><a href="/dentistry/patients/screeningform.php">Patient Screening Form</a></li>
+                <li><a href="/dentistry/udp/">University Dental Practice</a></li>
+                <li><a href="http://www.methodisthealth.org/utmp/">UT Methodist Physicians Home</a></li>
+                <li><a href="http://www.methodisthealth.org/patients-guests/find-a-physician/">Find a Doctor</a></li>
+                <li><a href="http://www.methodisthealth.org/patients-guests/">Patient Resources</a></li>
+                <li><a href="http://www.methodisthealth.org/healthcare-services/">Specialties Offered</a></li>
+                <li><a href="http://www.lebonheur.org/ulps/">UT Le Bonheur Pediatric Specialists Home</a></li>
+                <li><a href="http://www.lebonheur.org/your-visit/search-for-a-le-bonheur-physician/">Find a Pediatric Specialists Physician</a></li>
+                <li><a href="http://www.lebonheur.org/your-visit/our-regional-and-specialty-clinics/">Locations</a></li>
+                <li><a href="http://www.lebonheur.org/ulps/">Specialties Offered</a></li>
+                <li><a href="/bcdd/services/">Boling Center for Developmental Disabilities</a></li>
+                <li><a href="http://www.campbellclinic.com/">Campbell Orthopedic Clinic</a></li>
+                <li><a href="/eye/patient_resources.php">Hamilton Eye Institute</a></li>
+                <li><a href="/transplant/">Methodist University Transplant Institute</a></li>
+                <li><a href="http://www.semmes-murphey.com/">Semmes-Murphey Neurologic &amp; Spine Institute</a></li>
+                <li><a href="http://www.westclinic.com/">The West Clinic</a></li>
+                <li><a href="/univheal/">University Health Services</a></li>
+                <li><a href="/allied/uth/">University Therapists</a></li>
+            </ol>
+    Clinical Care (end)-->
+    <!--Public Service
+        <a class="button uthsc-split-button-home inverse" href="/publicservice/"><i class="fa fa-globe"></i></a>
+        <button data-dropdown="mission-publicservice" aria-controls="drop", aria-expanded="false" class="button dropdown uthsc-split-button inverse">Public Service</button><br>
+            <ol aria-labelledby="breadcrumblabel" id="mission-publicservice" data-dropdown-content class="f-dropdown mega uthsc-split-button-mission-links" role="menu" aria-hidden="false" tabindex="-1">
+                <li><i class="fa fa-level-up fa-flip-horizontal"></i><a href="/publicservice/" title="Home">Go to the Public Service Site</a></li>
+                <li><a href="/fooddrive/">Campus Food Drive</a></li>
+                <li><a href="/heartwalk/">Heart Walk</a></li>
+                <li><a href="/unitedway/">United Way Campaign</a></li>
+                <li><a href="http://bbhsl.mecca.org/">Building Bridges to Health Science Literacy</a></li>
+                <li><a href="/HCP/">Health Career Programs</a></li>
+                <li><a href="/prevmed/health_promo.php">Preventive Medicine Community Health Promotion</a></li>
+                <li><a href="/publicservice/clinica_esperanza.pdf">Clinica Esperanza (Clinic of Hope)</a></li>
+                <li><a href="/CIAO/">Council for International &amp; Area Outreach (CIAO)</a></li>
+            </ol>
+    Public Service (end)-->
+    <!--Mission Links (end)-->
 
     <ul>
-        <li><button class="close-menu">Close &rarr;</button></li>
-        <li><a href="#">Broccoli</a></li>
+        <li class="button menu"><i class="fa fa-gift"></i> Make a Gift</li>
+        <li class="button menu"><i class="fa fa-road"></i> Take a Tour</li>
+        <li>
+            <a href="#">Dropdown 1</a>
+            <ul class="menu-links">
+                <li><a href="#">First link in dropdown</a></li>
+                <li class="active"><a href="#">Active link in dropdown</a></li>
+            </ul>
+        </li>
         <li><a href="#">Tomato</a></li>
         <li><a href="#">Cucumber</a></li>
         <li><a href="#">Kale</a></li>
@@ -75,9 +194,8 @@
         <li><a href="#">Kale</a></li>
         <li><a href="#">Celery</a></li>
     </ul>
-    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
-        rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo.</p>
+    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        <button class="close-menu expand">&emsp; Close Menu &emsp;<i class="fa fa-chevron-right"></i></button>
 </nav>
 
 <!--Off Canvas Wrapper-->
@@ -92,7 +210,7 @@
     if (strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone')) {
         ?>
         <div class="bottom-nav-bar">&copy UTHSC 2014</div>
-        <?php
+    <?php
     }
     ?>
 </div>
