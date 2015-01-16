287b6f15595b751939c5aac15efa8c4a1f761d43
nicwinn <nic.liamg@gmail.com>
2015-01-16 12:44:38 -0600

    normalized data in the navigation list

--- index.php
+++ index.php
@@ -255,59 +255,69 @@
     <ul class="row collapse" data-equalizer data-uthscequalizer>
 
         <li class="uthsc-navigation-column small-2 columns">
-            <a href="#" data-equalizer-watch>Biomedical Education</a>
+            <a href="#" data-equalizer-watch>Menu Dropdown 0ne</a>
             <ul data-uthscequalizer-watch>
-                <li><a href="#">First link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
+                <li><a href="#">First Item in Dropdown</a></li>
+                <li><a href="#">Second Item in Dropdown</a></li>
+                <li><a href="#">Third Item in Dropdown</a></li>
+                <li><a href="#">Fourth Item in Dropdown</a></li>
+                <li><a href="#">Fifth Item in Dropdown</a></li>
             </ul>
         </li>
         <li class="uthsc-navigation-column small-2 columns">
-            <a href="#" data-equalizer-watch>Molecular Sciences</a>
+            <a href="#" data-equalizer-watch>Menu Dropdown Two</a>
             <ul data-uthscequalizer-watch>
-                <li><a href="#">First link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
-                <li><a href="#">First link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
+                <li><a href="#">First Item in Dropdown</a></li>
+                <li><a href="#">Second Item in Dropdown</a></li>
+                <li><a href="#">Third Item in Dropdown That Goes to Two Lines</a></li>
+                <li><a href="#">Fourth Item in Dropdown</a></li>
             </ul>
         </li>
         <li class="uthsc-navigation-column small-2 columns">
-            <a href="#" data-equalizer-watch>Faculty & Staff</a>
+            <a href="#" data-equalizer-watch>Menu Dropdown Three</a>
             <ul data-uthscequalizer-watch>
-                <li><a href="#">First link in dropdown link in dropdow link in dropdow</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
-                <li><a href="#">First link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
-                <li><a href="#">First link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
-                <li><a href="#">First link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
-                <li><a href="#">First link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
+                <li><a href="#">First Item in Dropdown</a></li>
+                <li><a href="#">Second Item in Dropdown</a></li>
+                <li><a href="#">Third Item in Dropdown</a></li>
+                <li><a href="#">Fourth Item in Dropdown</a></li>
+                <li><a href="#">Fifth Item in Dropdown</a></li>
+                <li><a href="#">Sixth Item in Dropdown</a></li>
+                <li><a href="#">Seventh Item in Dropdown</a></li>
+                <li><a href="#">Eighth Item in Dropdown</a></li>
+                <li><a href="#">Ninth Item in Dropdown</a></li>
+                <li><a href="#">Tenth Item in Dropdown</a></li>
+                <li><a href="#">Eleventh Item in Dropdown</a></li>
             </ul>
         </li>
         <li class="uthsc-navigation-column small-2 columns">
-            <a href="#" data-equalizer-watch>Neuro Scientific</a>
+            <a href="#" data-equalizer-watch>Menu Dropdown Four</a>
             <ul data-uthscequalizer-watch>
-                <li><a href="#">First link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
-                <li><a href="#">First link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
+                <li><a href="#">First Item in Dropdown</a></li>
+                <li><a href="#">Second Item in Dropdown</a></li>
+                <li><a href="#">Third Item in Dropdown</a></li>
+                <li><a href="#">Fourth Item in Dropdown</a></li>
+                <li><a href="#">Fifth Item in Dropdown</a></li>
+                <li><a href="#">Sixth Item in Dropdown</a></li>
+                <li><a href="#">Seventh Item in Dropdown</a></li>
             </ul>
         </li>
         <li class="uthsc-navigation-column small-2 columns">
-            <a href="#" data-equalizer-watch>Dropdown 5</a>
+            <a href="#" data-equalizer-watch>Menu Dropdown Five</a>
             <ul data-uthscequalizer-watch>
-                <li><a href="#">First link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
+                <li><a href="#">First Item in Dropdown</a></li>
             </ul>
         </li>
         <li class="uthsc-navigation-column small-2 columns">
-            <a href="#" data-equalizer-watch>Dropdown 6</a>
+            <a href="#" data-equalizer-watch>Menu Dropdown Six</a>
             <ul data-uthscequalizer-watch>
-                <li><a href="#">First link in dropdown</a></li>
-                <li class="active"><a href="#">Active link in dropdown</a></li>
+                <li><a href="#">First Item in Dropdown</a></li>
+                <li><a href="#">Second Item in Dropdown</a></li>
+                <li><a href="#">Third Item in Dropdown</a></li>
+                <li><a href="#">Fourth Item in Dropdown</a></li>
+                <li><a href="#">Fifth Item in Dropdown That Goes to Three Lines Because it Is Very Long</a></li>
+                <li><a href="#">Sixth Item in Dropdown</a></li>
+                <li><a href="#">Seventh Item in Dropdown</a></li>
+                <li><a href="#">Eighth Item in Dropdown</a></li>
             </ul>
         </li>
     </ul>
