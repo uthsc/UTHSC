e430680a677e577dbca949424890a9ca842f263c
nicwinn <nic.liamg@gmail.com>
2015-02-12 14:43:28 -0600

    Removed foundation's unused on-canvas for mobile menu

--- index.php
+++ index.php
@@ -218,11 +218,7 @@
 
     <!--Top Bar Navigation-->
     <nav class="top-bar show-for-large-up" data-topbar="data-topbar" role="navigation">
-        <ul class="title-area">
-            <li class="name"></li>
-            <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-            <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-        </ul>
+
 
         <!-- Left Top Bar Section -->
         <section class="top-bar-section left">
