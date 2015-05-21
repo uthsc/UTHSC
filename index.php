5ea5c67e6dd705af2a73b6b3492c0ffd007ee3f6
nicwinn <nic.liamg@gmail.com>
2015-05-21 10:45:01 -0500

    Pulled right off canvas menu out of index and template to be one file as include

--- index.php
+++ index.php
@@ -40,54 +40,7 @@
       </script>
     </div>
     <!--Right Off Canvas Menu-->
-    <nav class="offcanvas-nav menu push-menu-right"><a href="/" class="button uthsc-split-button-home"><i class="fa fa-home"></i></a>
-      <button data-dropdown="breadcrumbs-right" aria-controls="drop" aria-expanded="false" class="button dropdown uthsc-split-button">Breadcrumbs Back Home</button><br>
-      <ol id="breadcrumbs-right" aria-labelledby="breadcrumblabel" data-dropdown-content="data-dropdown-content" role="menu" aria-hidden="false" tabindex="-1" class="f-dropdown mega uthsc-split-button-breadcrumb-links">
-        <li><a href="/" title="Home"><i style="float: left;" class="fa fa-level-up fa-2x fa-flip-horizontal"></i>Back to the Homepage</a></li>
-        <li><a href="#" title="College of Medicine">College of Medicine</a></li>
-        <li><a href="#" title="Office of Medical Education">Office of Medical Education</a></li>
-        <li><a href="#" title="Clerkships">Clerkships</a></li>
-        <li><a href="#" title="Core Clerkship Directors">Core Clerkship Directors</a></li>
-        <li><a href="#"><strong>Current Page</strong></a></li>
-      </ol>
-      <div class="search-bar has-form mobile">
-        <input type="text" class="search"><a href="#" class="button secondary tiny"><i class="fa fa-search"></i></a>
-      </div>
-      <button class="close-menu expand">Close Menu &emsp;<i class="fa fa-chevron-right"></i>
-        <!-- Mission Links-->
-      </button>
-      <div class="mission-links">
-        <h2>Mission</h2><a href="/education/" class="button inverse"><i class="fa fa-graduation-cap"></i>Education</a><a href="/research/" class="button inverse"><i class="fa fa-flask"></i>Research</a><a href="/clinicalcare/" class="button inverse"><i class="fa fa-medkit"></i>Clinical Care</a><a href="/publicservice/" class="button inverse"><i class="fa fa-globe"></i>Public Service</a>
-      </div>
-      <ul>
-        <li class="button menu"><a href="#"><i class="fa fa-gift"></i>Make a Gift</a></li>
-        <li class="button menu"><a href="template.php"><i class="fa fa-road"></i>Take a Tour</a></li>
-        <li class="not-a-link"><a>Information for...</a>
-          <ul>
-            <li><a href="#">Students</a></li>
-            <li><a href="#">Faculty &amp; Staff</a></li>
-            <li><a href="#">Alumni</a></li>
-            <li><a href="#">Visitors</a></li>
-            <li><a href="#">Patients</a></li>
-          </ul>
-        </li>
-        <li class="not-a-link menu"><a><i class="fa fa-university">&emsp; Resources</i></a>
-          <ul>
-            <li><a href="#">Banner</a></li>
-            <li><a href="#">Blackboard</a></li>
-            <li><a href="#">Calendar</a></li>
-            <li><a href="#">Career Opportunities</a></li>
-            <li><a href="#">iLogin</a></li>
-            <li><a href="#">Maps</a></li>
-            <li><a href="#">MyUT</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">Webmail</a></li>
-          </ul>
-        </li>
-      </ul>
-      <button class="close-menu expand">Close Menu &emsp;<i class="fa fa-chevron-right"></i></button><?php if (strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone')) { ?>
-      <div class="bottom-nav-bar-off-canvas"></div><?php } ?>
-    </nav>
+    <?php include("template-files/right-off-canvas-menu.php"); ?>
     <!--Off Canvas Wrapper-->
     <div id="wrapper">
       <div id="mobile-navigation" class="hide-for-large-up">
