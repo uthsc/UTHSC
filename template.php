4b7e0603062576e6dd84c47a39242f1c0b990240
gpspake <gpspake@gmail.com>
2015-10-12 10:45:51 -0500

    fix page heading in content template

--- template.php
+++ template.php
@@ -319,44 +319,44 @@
             <p>Thanks to<a href="http://fortawesome.github.io/Font-Awesome/"> Fontawesome&nbsp;<span class="fa fa-flag"></span></a> we can use these handy icons anywhere. Here's an example of a<a href=""> pdf link&nbsp;<span class="fa fa-file-pdf-o"></span></a>. Sed ut<strong> perspiciatis</strong> unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,<strong> totam<em> rem</em> aperiam</strong>, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae</p>
             <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed qui consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
           </div>
-          <!-- Content Row 2-->
-          <div class="row content-area">
-            <div class="large-4 columns">
-              <h3>Content Row 2</h3>
-              <h5>One Third</h5>
-              <p><strong>This is a three column layout</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p><a href="#" class="button secondary radius">Apply Now&ensp;<span class="fa fa-graduation-cap"></span></a>
-            </div>
-            <div class="large-4 columns">
-              <h3>Content Row 2</h3>
-              <h5>One Third</h5>
-              <p><strong>This is a three column layout</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p><a href="#" class="button radius">Academic Calendar&ensp;<span class="fa fa-calendar"></span></a>
-            </div>
-            <div class="large-4 columns">
-              <h3>Content Row 2</h3>
-              <h5>UT President Joe DiPietro describes challenges facing higher ed and its role in preparing the workforce of the future</h5>
-              <figure class="left image-40"><img src="http://news.uthsc.edu/wp-content/uploads/2014/09/Dr.-Dipietro2.jpg" alt="UT President Joe DiPietro addresses panel members about the role of higher education in workforce preparedness.">
-                <figcaption>UT President Joe DiPietro addresses panel members about the role of higher education in workforce preparedness.</figcaption>
-              </figure>
-              <p><strong>This is a three column layout</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p><a href="#" class="button radius">Get Directions&ensp;<span class="fa fa-map-marker"></span></a>
-            </div>
-          </div>
-          <!-- Content Row 3-->
-          <div class="row content-area">
-            <div class="large-4 columns">
-              <h3>Content Row 3</h3>
-              <h5>One Third</h5>
-              <figure class="right image-40"><img src="http://news.uthsc.edu/wp-content/uploads/2014/12/Top-Workplace-HomePageSlider.jpg" alt="the arches on union avenue">
-                <figcaption>UTHSC named one of the 2014 Top Workplaces in Memphis.</figcaption>
-              </figure>
-              <p><strong>This is a two column layout</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-            </div>
-            <div class="large-8 columns">
-              <h3>Content Row 3</h3>
-              <h5>Two Thirds</h5>
-              <div class="left"><img src="_resources/2015/images/bridge.jpg"></div>
-              <p><strong>This is a two column layout</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-            </div>
-          </div>
+        </div>
+      </div>
+      <!-- Content Row 2-->
+      <div class="row content-area">
+        <div class="large-4 columns">
+          <h3>Content Row 2</h3>
+          <h5>One Third</h5>
+          <p><strong>This is a three column layout</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p><a href="#" class="button secondary radius">Apply Now&ensp;<span class="fa fa-graduation-cap"></span></a>
+        </div>
+        <div class="large-4 columns">
+          <h3>Content Row 2</h3>
+          <h5>One Third</h5>
+          <p><strong>This is a three column layout</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p><a href="#" class="button radius">Academic Calendar&ensp;<span class="fa fa-calendar"></span></a>
+        </div>
+        <div class="large-4 columns">
+          <h3>Content Row 2</h3>
+          <h5>UT President Joe DiPietro describes challenges facing higher ed and its role in preparing the workforce of the future</h5>
+          <figure class="left image-40"><img src="http://news.uthsc.edu/wp-content/uploads/2014/09/Dr.-Dipietro2.jpg" alt="UT President Joe DiPietro addresses panel members about the role of higher education in workforce preparedness.">
+            <figcaption>UT President Joe DiPietro addresses panel members about the role of higher education in workforce preparedness.</figcaption>
+          </figure>
+          <p><strong>This is a three column layout</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p><a href="#" class="button radius">Get Directions&ensp;<span class="fa fa-map-marker"></span></a>
+        </div>
+      </div>
+      <!-- Content Row 3-->
+      <div class="row content-area">
+        <div class="large-4 columns">
+          <h3>Content Row 3</h3>
+          <h5>One Third</h5>
+          <figure class="right image-40"><img src="http://news.uthsc.edu/wp-content/uploads/2014/12/Top-Workplace-HomePageSlider.jpg" alt="the arches on union avenue">
+            <figcaption>UTHSC named one of the 2014 Top Workplaces in Memphis.</figcaption>
+          </figure>
+          <p><strong>This is a two column layout</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+        </div>
+        <div class="large-8 columns">
+          <h3>Content Row 3</h3>
+          <h5>Two Thirds</h5>
+          <div class="left"><img src="_resources/2015/images/bridge.jpg"></div>
+          <p><strong>This is a two column layout</strong> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
         </div>
       </div>
       <footer>
